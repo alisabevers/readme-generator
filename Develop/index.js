9d7e1407cc0c652c2e6fbe97e7fe96c093357c27
A Bevers <alisabevers@gmail.com>
2023-04-26 18:28:18 -0400

    last push didn't ignore the node modules. Trying again.

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your Github username?', 'What is your email address?', 'What is the name of your project?', 'Please write a short description of your project.', 'What kind of license should your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user needs to know about contributing to the repo?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
