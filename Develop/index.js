ba6dd538208f041c2b1d7397445fc4c5e4061f23
A Bevers <alisabevers@gmail.com>
2023-04-27 15:28:30 -0400

    added a readme for the assigment

--- Develop/index.js
+++ Develop/index.js
@@ -1,69 +1,65 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your Github username?',
-            name: 'username',
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is the name of your project?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Please write a short description of your project.',
-            name: 'description',
-        },
-        {
-            type: 'list',
-            message: 'What kind of license should your project have?',
-            name: 'license',
-            choices: ['MIT', 'Apache', 'GPL', 'Other', 'N/A'],
-        },
-        {
-            type: 'input',
-            message: 'What command should be run to install dependencies?',
-            name: 'installation',
-        },
-        {
-            type: 'input',
-            message: 'What is the command to run tests?',
-            name: 'tests',
-        },
-        {
-            type: 'input',
-            message: 'What does the user need to know about using the repo?',
-            name: 'usage',
-        },
-        {
-            type: 'input',
-            message: 'What does the user needs to know about contributing to the repo?',
-            name: 'contribution',
-        }
-    ])
-    .then(({
-        title,
-        description,
-        installation,
-        instructions,
-        credit,
-        license,
-        username,
-        email,
-        usage,
-        contribution
-    })=>{ const template = 
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'GPL', 'Other', 'N/A'],
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the command to run tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What does the user needs to know about contributing to the repo?',
+        name: 'contribution',
+    }
+]).then(({
+    title,
+    description,
+    installation,
+    instructions,
+    credit,
+    license,
+    username,
+    email,
+    usage,
+    contribution
+})=>{ const template = 
 
 `# ${title}
 
@@ -79,7 +75,7 @@ ${installation}
 ## Contribution 
 ${contribution}
 
-## instructions 
+## Instructions 
 ${instructions}
 
 ## Credits 
@@ -94,8 +90,9 @@ ${username}
 * Email: 
 ${email}`;
         
-        createNewFile(title,template);
-    });
+createNewFile(title,template);
+});
+
 
 function createNewFile(fileName, data){
     fs.writeFile(`./${fileName}.md`, data, (err)=> {
